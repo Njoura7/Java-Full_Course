e99edd583f56fa0e047410cdef71eaba34e476a9
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-10 20:34:56 +0200

    while, do while loops syntax

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
-
+import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args){
+    public static void main(String[] args) {
+        //while loop execute a block of code as along as its condition remains true
 
-       System.out.println("Hello Java!");
+        Scanner nameScanner = new Scanner(System.in);
+        String name = "";
 
-   }
+        while (name.isBlank()) {
+            System.out.println("enter your name");
+            name = nameScanner.nextLine();
+        }
+        System.out.println("hello " + name);
+
+//the difference with the do while syntax is that the block of code is at least executed once
+            do{
+                System.out.println("enter your name");
+                name = nameScanner.nextLine();
+            }while (name.isBlank());
+        System.out.println("hello again " + name);
+    }
 }
 
