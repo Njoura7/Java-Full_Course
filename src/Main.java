837ca9278703717878e704a2ac1409e9fcdee3bc
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-10 12:22:01 +0200

    test github workflow

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 
 public class Main {
    public static void main(String[] args){
-
-       System.out.println("Hello Java!");
+       //first commit in 9/If-statement branch to test github workflow
+       System.out.println("test github workflow");
 
    }
 }
