77ac26d5437bd672c853175319a90bb9f202ab2a
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-10 14:30:59 +0200

    switch syntax

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,24 @@
 
 public class Main {
    public static void main(String[] args){
-//test github workflow
-       System.out.println("Hello Switches!");
+// switch==> statement that allows a variable to be tested for equality against a list of values
+       String day ="Friday";
+
+       switch (day){
+           case "Sunday":
+               System.out.println("it's sunday!!");
+               //break is important so that the rest of code will not be executed if the case is matched
+               break;
+           case "Monday":
+               System.out.println("Monday is here :)");
+               break;
+           case "Friday":
+               System.out.println("joumou3a moubarka");
+               break;
+               //if there is no match
+           default:
+               System.out.println("not a day");
+       }
 
    }
 }
