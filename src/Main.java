389a703b3ae1494853f2a7c6c9bccf43d34248db
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-11 19:43:23 +0200

    arrays[]..

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,33 @@
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+       //array==> used to store multiple values in a single variable
+
+       //values have top be the same data type og the string
+       String [] cars ={"Camaro","Corvette","Tesla"};
+
+      //reassign a value within the array
+       cars[0] = "Mustang";
+
+       //print the second value of the array, since the index start with 0 always
+       System.out.println(cars[1]);
+
+       //Array index is out of bounds
+//       System.out.println(cars[4]);
+
+       //other method of creating arrays
+
+       int[] numbers =new int[5];
+       //just random numbers
+       numbers[0]=13;
+       numbers[1]=6;
+       numbers[2]=1999;
+       numbers[3]=7;
+       numbers[4]=4;
+
+       for(int i=0;i<= numbers.length;i++){
+           System.out.println(numbers[i]);
+       }
 
    }
 }
