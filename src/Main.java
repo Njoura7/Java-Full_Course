e67654a2075e62a26cf134e0c3873781a455503c
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-11 19:29:59 +0200

    nested loops

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+       Scanner scanner = new Scanner(System.in);
+
+       int rows;
+       int columns;
+       String symbol = "";
+
+       System.out.println("Enter number of rows");
+       rows=scanner.nextInt();
+
+       System.out.println("Enter number of columns");
+       columns=scanner.nextInt();
+
+       System.out.println("Enter your symbol");
+       symbol=scanner.next();
+
+       for (int i=1;i<=rows;i++){
+           System.out.println();
+           for (int j=1;j<=columns;j++){
+               System.out.print(symbol);
+           }
+       }
 
    }
 }
