ca1324592cd7ae31ab4e9847a3b7a0cb07bc7079
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-11 19:14:36 +0200

    for loops syntax

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 
 
 public class Main {
-   public static void main(String[] args){
+   public static void main(String[] args) {
 
-       System.out.println("Hello Java!");
+       // for loop ==> executes a block of a code for a limited amount of times based on a condition
 
+       //the incrementation can be customizable; the most known ones are i++ and i-- but it can be set to i+=3 or i-=2 for example to control the step dor each increment
+
+//       for(declare index;condition to stop;increment or decrement){
+//           block of code to execute
+//       }
+
+       for (int i=0; i <= 10; i++) {
+           System.out.println(i+" Hello for loop");
+       }
+       for(int i=10;i>=0;i-=2){
+           System.out.println(i);
+
+       }
+       System.out.println("end of loop");
    }
 }
 
