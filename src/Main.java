9c3cc389de0372f264fadc6a17eb0189fadba550
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-11 21:20:01 +0200

    2d array or matrix

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,31 @@
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+       // 2D array = an array of arrays
 
+       String[][] sports = new String[2][2];
+
+       //declaring the content of our 2d array
+
+       sports[0][0]="Tennis";
+       sports[0][1]="Ping-pong";
+       sports[1][0]="Volley-ball";
+       sports[1][1]="Football";
+
+       //alternative way to declare arrays
+
+//       String[][] sports=   {
+//                                {"Tennis","Ping-pong"},
+//                                {"Volley-ball","Football"}
+//                            };
+
+       //enhanced for or forEach syntax
+       for (String[] sport : sports) {
+           System.out.println();
+           for (int j = 0; j < sport.length; j++) {
+               System.out.print(sport[j] + " ");
+           }
+       }
    }
 }
 
