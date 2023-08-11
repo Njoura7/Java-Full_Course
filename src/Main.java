2fd57e8e9a4e1761ddbd54c8725265e6feedb15e
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-11 21:50:19 +0200

    basic sting methods

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,29 @@
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+       //String==> a reference data type that can store one or more characters(or number or symbol)
+       // reference data types have access to useful methods
+
+       String name = "Njoura";
+
+//       uncomment each line separately to view result in the console and don't forget to comment the right side of each line which explain each method
+
+
+//       boolean result = name.equals("Njoura"); ==> return a bool. and it's case-sensitive
+//       boolean result = name.equalsIgnoreCase("njoura"); ==> return a bool. but it is case-insensitive
+//       int result =name.length();==>return the number of elements of the string
+//       char result=name.charAt(5); ==>Returns the char value at the specified index.
+//       int result= name.indexOf("a"); ==>Returns the index within this string of the first occurrence of the specified substring
+//       boolean result=name.isBlank(); ==>Returns true if the string is empty or contains only white space codepoints, otherwise false.
+//       boolean result=name.isEmpty(); ==>Returns true if, and only if, length() is 0.
+//      String result = name.toUpperCase(); ==>Converts all of the characters in this String to upper case using the rules of the default locale. This method is equivalent to toUpperCase(Locale.getDefault()).
+//      String result =name.toLowerCase();==>Converts all of the characters in this String to lower case using the rules of the default locale
+//       String result= name.trim(); ==>Returns a string whose value is this string, with all leading and trailing space removed, where space is defined as any character whose codepoint is less than or equal to 'U+0020' (the space character)
+//        String result= name.replace('a','*'); ==>Returns a string resulting from replacing all occurrences of oldChar in this string with newChar.
+
+       System.out.println(result);
+
+
 
    }
 }
