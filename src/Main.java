cd94b8955fb42fe13a8065e16b0f0087996e0b97
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-12 16:06:30 +0200

    wrapper classes,autoboxing,unboxing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,37 @@
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+     //wrapper class==> provides a way to use primitive date types as reference data types
+                       //reference data types contains useful methods
+                       //can be used with collections (ex.ArrayList)
+
+       //primitives     //wrapper
+       //----------    //---------
+//       boolean           Boolean
+//       char              Character
+//       int               Integer
+//       double            Double
+
+// primitive=>wrapper class      */Autoboxing ==> the automatic conversion that the Java compiler makes between the primitive types and their corresponding object wrapper classes
+//wrapper class=> primitive    */unboxing ==> the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+       Boolean b = true;
+       Character c = '@';
+       Integer i = 123;
+       Double d = 3.14;
+
+       String s = "Njoura";
+
+//      Now, we can access to methods simply by following the name of the variable with a point
+//some examples:
+//       b.booleanValue();
+//       c.toString();
+//       i.byteValue();
+//       d.floatValue();
+//
+
+
+
 
    }
 }
