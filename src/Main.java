f1230fe079ef1f0719b01ec8ba88521b3ac30da4
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-12 16:19:06 +0200

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
-
+import java.util.ArrayList;
 
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+       ArrayList<String> food = new ArrayList<String>();
+
+       food.add("pizza");
+       food.add("3ejja");
+       food.add("mosli");
+//       set (index,element) ==>Replaces the element at the specified position in this list with the specified element
+       food.set(2,"ma9arouna");
+//remove the specified element based on the index
+       food.remove(1);
+
+       //to clear our list we can use :
+//       food.clear();
+
+       for (int i=0;i<food.size();i++){
+           System.out.print(food.get(i)+" *");
+       }
 
    }
 }
