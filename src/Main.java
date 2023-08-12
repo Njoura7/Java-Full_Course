a4d25edb9d8cbcef594cf9bd69c88cdb4846ca1e
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-13 00:09:12 +0200

    2D-ArrayList-list of lists

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
-
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+        // 2D ArrayList ==> a dynamic list of lists
+       //You can change the size of these lists during runtime
+
+
+      ArrayList<ArrayList<String>> groceryList=new ArrayList<>();
+
+      ArrayList<String> veggiesList = new ArrayList<>();
+      veggiesList.add("tomatoes");
+      veggiesList.add("peppers");
+      veggiesList.add("onion");
+
+      ArrayList<String> drinksList = new ArrayList<>();
+      drinksList.add("code");
+      drinksList.add("coffee");
+
+      ArrayList<String> bakeryList = new ArrayList<>();
+      bakeryList.add("pizza");
+
+      //filling the groceryList
+      groceryList.add(bakeryList);
+      groceryList.add(veggiesList);
+      groceryList.add(drinksList);
+
+      System.out.println(drinksList);
+      System.out.println(groceryList); //[[pizza], [tomatoes, peppers, onion], [code, coffee]]
 
    }
 }
