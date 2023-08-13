c367376ac6762c4d635ffcbf8a6ea5e8d644faa8
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-13 20:29:11 +0200

    forrach loop/enhanced loop syntax

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
-
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main {
    public static void main(String[] args){
 
-       System.out.println("Hello Java!");
+    //for-each ==> traversing technique to iterate through the elements in an array/collection
+       // less steps, more readable
+       //less flexible
 
+
+       String[] animals ={"cat","dog","horse"};
+       for(String i : animals){
+           System.out.println(i);
+       }
+       System.out.println("********");
+       ArrayList<String> jobs=new ArrayList<>();
+
+       jobs.add("web developer");
+       jobs.add("freelancer");
+       jobs.add("customer service");
+       for(String i : jobs){
+           System.out.println(i);
+       }
    }
 }
 
